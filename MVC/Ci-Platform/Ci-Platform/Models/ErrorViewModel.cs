eb68ac5a810b10c34c3b2b3f45ee84bd7f01b8e3
yashnarola722 <yashnarola722@gmail.com>
2023-02-13 18:21:55 +0530

    Mvc added

---
+++ MVC/Ci-Platform/Ci-Platform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ci_Platform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
